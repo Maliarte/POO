9dcf0e4bde676259f3da8e85a5834cf670d62c43
Marília S <42978269+Maliarte@users.noreply.github.com>
2023-06-04 18:13:13 -0300

    coment

--- Hortifruti/src/Produto.java
+++ Hortifruti/src/Produto.java
@@ -1,17 +1,17 @@
 abstract class Produto {
-    private int quantidade;
+    private int quantidade; // Variável de instância que armazena a quantidade do produto
 
-    public Produto(int quantidade) {
-        this.quantidade = quantidade;
+    public Produto(int quantidade) {// Construtor que recebe a quantidade do produto como argumento
+        this.quantidade = quantidade;// Atribui o valor da quantidade passada como argumento à variável de instância
     }
 
-    public abstract double getPreco();
+    public abstract double getPreco();  // Método abstrato para obter o preço do produto
 
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
+    public void setQuantidade(int quantidade) { // Método para definir a quantidade do produto
+        this.quantidade = quantidade // Atribui o valor da quantidade passada como argumento à variável de instância
     }
 
-    public int getQuantidade() {
-        return quantidade;
+    public int getQuantidade() { // Método para obter a quantidade do produto
+        return quantidade; // Retorna o valor da quantidade do produto
     }
 }
