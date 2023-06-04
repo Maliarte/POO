e504072a1ef40ed64f0cc4a2c15c174dedb500fa
Marília S <42978269+Maliarte@users.noreply.github.com>
2023-06-04 18:29:40 -0300

    coment

--- Hortifruti/src/Main.java
+++ Hortifruti/src/Main.java
@@ -24,84 +24,119 @@
  * 
  */
 
- import java.util.Scanner;
+public static void main(String[] args) {
+    // Cria um objeto Scanner para ler a entrada do usuário
+    Scanner scanner = new Scanner(System.in);
+ 
+    // Cria um objeto Cesta para armazenar os produtos
+    Cesta cesta = new Cesta();
 
- public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Cesta cesta = new Cesta();
+    int opcao;
+    // Inicia um loop que será executado até que a opção 0 (Sair) seja escolhida
+    do {
+        // Mostra o menu de opções para o usuário
+        System.out.println("Menu de opções:");
+        System.out.println("1. Colocar produto na cesta");
+        System.out.println("2. Mostrar conteúdo da cesta e valor total");
+        System.out.println("3. Limpar cesta");
+        System.out.println("0. Sair");
+        System.out.print("Digite a opção desejada: ");
+     
+        // Lê a opção digitada pelo usuário
+        opcao = scanner.nextInt();
+     
+        System.out.println();
 
-        int opcao;
-        do {
-            System.out.println("Menu de opções:");
-            System.out.println("[1]Colocar produto na cesta");
-            System.out.println("[2]Mostrar conteúdo da cesta e valor total");
-            System.out.println("[3]Limpar cesta");
-            System.out.println("[0]Sair");
-            System.out.print("Digite a opção desejada: ");
-            
-            opcao = scanner.nextInt();
-            System.out.println();
-
-            switch (opcao) {
-                case 1:
-                    System.out.println("Tipos de produtos:");
-                    System.out.println("1. Banana");
-                    System.out.println("2. Maçã");
-                    System.out.println("3. Pera");
-                    System.out.println("4. Uva");
-                    System.out.println("5. Morango");
-                    System.out.println("6. Limão");
-                    System.out.println("7. Mamão");
-                    System.out.print("Digite o número do produto desejado: ");
-                    int tipo = scanner.nextInt();
-                    System.out.print("Digite a quantidade: ");
-                    int quantidade = scanner.nextInt();
-                    Produto produto = criarProduto(tipo, quantidade);
-                    if (produto != null) {
-                        cesta.adicionarProduto(produto);
-                    }
-                    System.out.println();
-                    break;
-                case 2:
-                    cesta.listarCesta();
-                    System.out.println();
-                    break;
-                case 3:
-                    cesta.limparCesta();
-                    System.out.println();
-                    break;
-                case 0:
-                    System.out.println("Encerrando o programa...");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Digite um número válido.");
-                    System.out.println();
-                    break;
-            }
-        } while (opcao != 0);
-
-        scanner.close();
-    }
-    private static Produto criarProduto(int tipo, int quantidade) {
-        switch (tipo) {
+        // Utiliza um switch para executar o código correspondente à opção escolhida
+        switch (opcao) {
             case 1:
-                return new Banana(quantidade);
+                System.out.println("Tipos de produtos:");
+                System.out.println("1. Banana");
+                System.out.println("2. Maçã");
+                System.out.println("3. Pera");
+                System.out.println("4. Uva");
+                System.out.println("5. Morango");
+                System.out.println("6. Limão");
+                System.out.println("7. Mamão");
+                System.out.print("Digite o número do produto desejado: ");
+          
+                // Lê o tipo de produto escolhido pelo usuário
+                int tipo = scanner.nextInt();
+          
+                System.out.print("Digite a quantidade: ");
+          
+                // Lê a quantidade do produto escolhida pelo usuário
+                int quantidade = scanner.nextInt();
+          
+                // Cria um objeto Produto chamando o método criarProduto com o tipo e a quantidade escolhidos
+                Produto produto = criarProduto(tipo, quantidade);
+          
+                // Verifica se o produto não é nulo (ou seja, foi criado corretamente)
+                if (produto != null) {
+                 
+                    // Adiciona o produto à cesta
+                    cesta.adicionarProduto(produto);
+                 
+                }
+                System.out.println();
+                break;
+          
             case 2:
-                return new Maca(quantidade);
+                // Lista os produtos presentes na cesta e mostra o valor total
+                cesta.listarCesta();
+                System.out.println();
+                break;
+          
             case 3:
-                return new Pera(quantidade);
-            case 4:
-                return new Uva(quantidade);
-            case 5:
-                return new Morango(quantidade);
-            case 6:
-                return new Limao(quantidade);
-            case 7:
-                return new Mamao(quantidade);
+                // Limpa a cesta, removendo todos os produtos
+                cesta.limparCesta();
+                System.out.println();
+                break;
+          
+            case 0:
+                // Encerra o programa
+                System.out.println("Encerrando o programa...");
+                break;
+          
             default:
-                System.out.println("Tipo de produto inválido.");
-                return null;
-            }
+                // Caso a opção digitada não seja válida, exibe uma mensagem de erro
+                System.out.println("Opção inválida. Digite um número válido.");
+                System.out.println();
+                break;
         }
+    } while (opcao != 0);
+
+    // Fecha o Scanner
+    scanner.close();
+}
+
+private static Produto criarProduto(int tipo, int quantidade) {
+    // Utiliza um switch para criar o objeto de produto correspondente ao tipo escolhido
+    switch (tipo) {
+        case 1:
+            // Cria e retorna um objeto Banana com a quantidade escolhida
+            return new Banana(quantidade);
+        case 2:
+            // Cria e retorna um objeto Maca com a quantidade escolhida
+            return new Maca(quantidade);
+        case 3:
+            // Cria e retorna um objeto Pera com a quantidade escolhida
+            return new Pera(quantidade);
+        case 4:
+            // Cria e retorna um objeto Uva com a quantidade escolhida
+            return new Uva(quantidade);
+        case 5:
+            // Cria e retorna um objeto Morango com a quantidade escolhida
+            return new Morango(quantidade);
+        case 6:
+            // Cria e retorna um objeto Limao com a quantidade escolhida
+            return new Limao(quantidade);
+        case 7:
+            // Cria e retorna um objeto Mamao com a quantidade escolhida
+            return new Mamao(quantidade);
+        default:
+            // Caso o tipo escolhido não seja válido, exibe uma mensagem de erro e retorna null
+            System.out.println("Tipo de produto inválido.");
+            return null;
     }
+}
